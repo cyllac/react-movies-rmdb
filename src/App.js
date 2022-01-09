97b73a68999a794f382b1c0872d383b4a0106df3
Priscila <cyllac@gmail.com>
2022-01-09 18:07:38 -0300

    Login e Pontuação do filme implementado.

--- src/App.js
+++ src/App.js
@@ -11,18 +11,25 @@ import Header from './components/Header';
 import Home from './components/Home';
 import Movie from './components/Movie';
 import NotFound from './components/NotFound';
+import Login from './components/Login';
+
+// Context
+import UserProvider from './context';
 
 const App = () => (
     <Router>
-      <Header />
+      <UserProvider>
+        <Header />
 
-      <Routes>
-        <Route path='/' element={<Home />}></Route>
-        <Route path='/:movieId' element={<Movie />}></Route>
-        <Route path='/*' element={<NotFound />}></Route>
-      </Routes>
+        <Routes>
+          <Route path='/' element={<Home />}></Route>
+          <Route path='/login' element={<Login />}></Route>
+          <Route path='/:movieId' element={<Movie />}></Route>
+          <Route path='/*' element={<NotFound />}></Route>
+        </Routes>
 
-      <GlobalStyle />
+        <GlobalStyle />
+      </UserProvider>
     </Router>
 );
 
